1528db2601ea379191bfa7623ae3c4889ac54774
Mifsolan79 <mifsut@protonmail.com>
2026-03-26 13:15:34 +0100

    Cleanup: Remove obsolete scripts and redundant files

--- check_bin.js
+++
@@ -1,6 +0,0 @@
-
-const fs = require('fs');
-const filePath = 'D:\\- - - GENERAR PROMPS\\PELICULAS\\data_prompts.js';
-const buffer = fs.readFileSync(filePath);
-console.log('Ultimos 100 bytes (hex):', buffer.slice(-100).toString('hex'));
-console.log('Ultimos 100 bytes (ASCII):', buffer.slice(-100).toString('utf8'));
